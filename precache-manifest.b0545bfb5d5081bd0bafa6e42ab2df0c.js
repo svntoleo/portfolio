a5f689bd26520cfa164d18e37def26f4f66608aa
Leo <svntoleo@gmail.com>
2020-11-01 16:34:08 -0300

    deploy

---
+++ precache-manifest.b0545bfb5d5081bd0bafa6e42ab2df0c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93cbe37f4369ddfd1da",
+    "url": "/portfolio/css/app.0c4629b8.css"
+  },
+  {
+    "revision": "f11b8f11d60bca6ee76c0f4f473735aa",
+    "url": "/portfolio/img/js.f11b8f11.png"
+  },
+  {
+    "revision": "9acfe78b8e1cfbf4c1b1d1f31745b96b",
+    "url": "/portfolio/img/pic.9acfe78b.png"
+  },
+  {
+    "revision": "286118381fe1f7124ad828615103a24a",
+    "url": "/portfolio/img/system.28611838.jpg"
+  },
+  {
+    "revision": "078926c022ae2322a799760cabcc4400",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b93cbe37f4369ddfd1da",
+    "url": "/portfolio/js/app.6567ea32.js"
+  },
+  {
+    "revision": "59a1f6e5d604d37e0cfb",
+    "url": "/portfolio/js/chunk-vendors.a37366e6.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
